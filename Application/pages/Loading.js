f944b25810023197ca3caa9394a7dc69455096ad
Tahreak <tahreak@Tahreaks-MacBook-Pro.local>
2020-10-17 22:02:11 -0400

    ' -> "

--- Application/pages/Loading.js
+++ Application/pages/Loading.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
 class Loading extends React.Component {
   render() {
@@ -14,7 +14,7 @@ class Loading extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
 })
 
--- Application/pages/Rosters.js
+++ Application/pages/Rosters.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
 class Rosters extends React.Component {
   render() {
@@ -14,7 +14,7 @@ class Rosters extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
 })
 
