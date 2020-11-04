1f4781a2a6bc7622d17ac76671bd894b6484a460
Tahreak <tahreak@Tahreaks-MacBook-Pro.local>
2020-11-03 22:45:37 -0500

    Front End + Mailing Complete

---
+++ Application/components/Feedback.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Dimensions, StyleSheet, View, TextInput, Alert, Button } from "react-native";
+import email from "react-native-email";
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+const scale = SCREEN_WIDTH / 320;
+
+class Feedback extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ""
+        }
+    }
+
+    submitFeedback = () => {
+        email("trtnufsports@gmail.com", {
+            subject: "User Feedback",
+            body: this.state.value
+        })
+        this.setState({value: ""})
+        Alert.alert("Message Sent", " Thank you for your feedback!")
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.textInput}
+                    multiline={true}
+                    padding={10}
+                    onChangeText={(e) => this.setState({value: e})}
+                    value={this.state.value}
+                />
+                <Button title="Submit" onPress={() => this.submitFeedback()} disabled={this.state.value.length === 0}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: "5%"
+    },
+    textInput: {
+        height: SCREEN_HEIGHT*0.25, 
+        borderRadius: 10,
+        backgroundColor: "white",
+        borderColor: "black",
+        borderWidth: 1
+    }
+})
+
+export default Feedback;
--- Application/pages/Settings.js
+++ Application/pages/Settings.js
@@ -1,21 +1,114 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Dimensions, StyleSheet, View, Text, Switch, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from "react-native";
+
+import Feedback from "../components/Feedback.js";
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+const scale = SCREEN_WIDTH / 320;
 
 class Settings extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pushNotificationsIsEnabled: false,
+      darkModeIsEnabled: false
+    }
+  }
+
+  togglePushNotifications = () => {
+    this.setState({
+      ...this.state,
+      pushNotificationsIsEnabled: !this.state.pushNotificationsIsEnabled
+    })
+  }
+
+  toggleDarkMode = () => {
+    this.setState({
+      ...this.state,
+      darkModeIsEnabled: !this.state.darkModeIsEnabled
+    })
+  }
+
   render() {
+    const keyboardVerticalOffset = Platform.OS === 'ios' ? 60 : 0
     return (
-      <View style={styles.container}>
-        <Text>Settings Here!</Text>
-      </View>
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={keyboardVerticalOffset}>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+          <View style={styles.container}>
+            <View>
+              <Text style={styles.headerText}>Version 1.1</Text>
+            </View>
+
+            <View style={styles.line}/>
+
+            <View>
+              <View style={styles.containerWithButton}>
+                <Text style={styles.headerText}>Push Notifications</Text>
+                <Switch
+                  trackColor={{false: "#767577", true: "#008000"}}
+                  onValueChange={this.togglePushNotifications}
+                  value={this.state.pushNotificationsIsEnabled}
+                  style={{alignSelf: "flex-end"}}
+                />
+              </View>
+            </View>
+
+            <View style={styles.line}/>
+
+            <View>
+            <View style={styles.containerWithButton}>
+                <Text style={styles.headerText}>Dark Mode</Text>
+                <Switch
+                  trackColor={{false: "#767577", true: "#008000"}}
+                  onValueChange={this.toggleDarkMode}
+                  value={this.state.darkModeIsEnabled}
+                  style={{alignSelf: "flex-end"}}
+                />
+              </View>
+            </View>
+
+            <View style={styles.line}/>
+
+            <View>
+              <Text style={styles.headerText}>About</Text>
+              <View style={{height: SCREEN_HEIGHT*0.25}}>
+                <Text style={styles.about}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+              </View>
+            </View>
+
+            <View style={styles.line}/>
+
+            <View>
+              <Text style={styles.headerText}>Submit Feedback</Text>
+              <Feedback/>
+            </View> 
+          </View>
+        </TouchableWithoutFeedback>  
+      </KeyboardAvoidingView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center"
+    padding: "5%",
   },
+  containerWithButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontSize: 18 * scale,
+    alignSelf: "flex-start"
+  },
+  about: {
+    margin: 10
+  },
+  line: {
+    height: 1,
+    backgroundColor: "black",
+    marginVertical: 10
+  }
 })
 
 export default Settings;
